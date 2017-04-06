72d7af4f96bafe8ef94d3a2027dde94cb2cd5462
Rachel W <skyjutsu@gmail.com>
2017-04-05 17:35:42 -0700

    Increment and decrement buttons now alter the quantity

--- MenuScript.js
+++ MenuScript.js
@@ -3,17 +3,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var incrementButtons = document.querySelectorAll(".increment-btn");
   var decrementButtons = document.querySelectorAll(".decrement-btn");
 
+  // Creates an event listener for the increment buttons.
   for (var i = 0; i < incrementButtons.length; i++) {
     incrementButtons[i].addEventListener("click", function() {
-      alert("You clicked an increment button.");
+      var quantityField = this.parentNode.getElementsByClassName("quantity-field")[0].getElementsByTagName("p")[0];
+      var quantity = Number(quantityField.innerHTML);
+      quantity = quantity + 1;
+      quantityField.innerHTML = String(quantity)
     })
   }
 
+  // Creates an event listener for the decrement buttons.
   for (var i = 0; i < decrementButtons.length; i++) {
     decrementButtons[i].addEventListener("click", function() {
-      alert("You clicked a decrement button.")
+      var quantityField = this.parentNode.getElementsByClassName("quantity-field")[0].getElementsByTagName("p")[0];
+      var quantity = Number(quantityField.innerHTML);
+
+      if (quantity > 0) {
+        quantity = quantity - 1;
+        quantityField.innerHTML = String(quantity)
+      }
     })
   }
-
 
 });
