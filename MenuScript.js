63d3230da4ddffdda12f0840207db1c705ae5c00
Rachel W <skyjutsu@gmail.com>
2017-04-05 17:12:50 -0700

    Increment and decrement buttons now give alert responses

--- MenuScript.js
+++ MenuScript.js
@@ -1,13 +1,19 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
   var incrementButtons = document.querySelectorAll(".increment-btn");
+  var decrementButtons = document.querySelectorAll(".decrement-btn");
 
   for (var i = 0; i < incrementButtons.length; i++) {
     incrementButtons[i].addEventListener("click", function() {
-      alert("You clicked the button.")
+      alert("You clicked an increment button.");
     })
   }
 
+  for (var i = 0; i < decrementButtons.length; i++) {
+    decrementButtons[i].addEventListener("click", function() {
+      alert("You clicked a decrement button.")
+    })
+  }
 
 
 });
