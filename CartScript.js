f1afdd87cd1f878f7cba09945c3bf0828cffbc3f
Rachel W <skyjutsu@gmail.com>
2017-04-12 17:29:38 -0700

    Cart page now updates cart on server

--- CartScript.js
+++ CartScript.js
@@ -1,5 +1,6 @@
 // Takes the items stored in the cart and displays them to the page
 // which includes the checkout options.
+var globalCart;
 
 // Event listener for when the cart page loads
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -7,11 +8,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // List items from the cart in the order-summary.
   $.get("http://thiman.me:1337/cart/Rachel", function(response) {
     var cart = serverCartToClientCart(response[response.length - 1]);
+    globalCart = response[0];
     var orderSummary = document.getElementsByClassName("order-summary")[0];
 
     for (var item in cart) {
       if(item != "totalItems" && item != "totalPrice") {
         var itemEntry = document.createElement("tr");
+        itemEntry.className = "item-entry";
 
         var dataEntry = document.createElement("td");
         dataEntry.innerHTML = String(item);
@@ -50,6 +53,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     dataEntry = document.createElement("td");
     dataEntry.innerHTML = "$" + cart["totalPrice"];
     dataEntry.className = "total price";
+    dataEntry.id = "total-price";
     itemEntry.appendChild(dataEntry);
 
     orderSummary.appendChild(itemEntry);
@@ -66,19 +70,59 @@ document.addEventListener("DOMContentLoaded", function(event) {
   $('.order-summary').on('click', '.increment-btn', function(event) {
     var quantityField = this.parentNode.getElementsByClassName("quantity-field")[0];
     var quantity = Number(quantityField.innerHTML);
+    var itemName = this.parentNode.parentNode.parentNode.getElementsByClassName("item-name")[0].innerHTML;
     quantity = quantity + 1;
+    globalCart[itemName + "[quantity]"] = quantity;
     quantityField.innerHTML = String(quantity);
+
+    console.log(globalCart);
+    globalCart[itemName + "[price]"] = Number(globalCart[itemName + "[price]"]) + Number(globalCart[itemName + "[unitPrice]"]);
+    globalCart["totalPrice"] = Number(globalCart["totalPrice"]) + Number(globalCart[itemName + "[unitPrice]"]);
+    var totalPriceField = document.querySelector('#total-price');
+    totalPriceField.innerHTML = "$" + globalCart["totalPrice"];
+
+    globalCart["totalItems"] = Number(globalCart["totalItems"]) + 1;
+    var totalItemField = document.querySelector('#total-items');
+    totalItemField.innerHTML = globalCart["totalItems"];
+    console.log(globalCart);
+
   }); // End of code for incrementing quantity to add
 
   // Creates an event listener for the decrement buttons.
   $('.order-summary').on('click', '.decrement-btn', function(event) {
     var quantityField = this.parentNode.getElementsByClassName("quantity-field")[0];
+    var itemName = this.parentNode.parentNode.parentNode.getElementsByClassName("item-name")[0].innerHTML;
     var quantity = Number(quantityField.innerHTML);
 
     if (quantity > 0) {
         quantity = quantity - 1;
+        globalCart[itemName + "[quantity]"] = quantity;
         quantityField.innerHTML = String(quantity);
+
+        console.log(globalCart);
+        globalCart[itemName + "[price]"] = Number(globalCart[itemName + "[price]"]) - Number(globalCart[itemName + "[unitPrice]"]);
+        globalCart["totalPrice"] = Number(globalCart["totalPrice"]) - Number(globalCart[itemName + "[unitPrice]"]);
+        var totalPriceField = document.querySelector('#total-price');
+        totalPriceField.innerHTML = "$" + globalCart["totalPrice"];
+
+        globalCart["totalItems"] = Number(globalCart["totalItems"]) - 1;
+        var totalItemField = document.querySelector('#total-items');
+        totalItemField.innerHTML = globalCart["totalItems"];
+        console.log(globalCart);
     }
   }); // End of code for decrementing quantity to add
+
+  // Update the cart with the current values whenever
+  // the user clicks the "Update Cart" button.
+  $('.order-summary').on('click', '.update-btn', function() {
+    // Put in code to replace the cart on the server.
+    $.ajax({
+      url: "http://thiman.me:1337/cart/Rachel/" + globalCart["_id"],
+      data: globalCart,
+      type: "PATCH",
+      dataType: "json"
+    });
+
+  }); // End of code for updating cart
 
 }) // End of event listner
